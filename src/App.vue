eb47a74bc09938a7cc18850e1c8f7cfe1f8f9597
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-01-03 10:23:47 +0100

    new lessons

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
+    <h1>{{ header }}</h1>
+    <!-- Podwójne {{ variable }} onazczają wyrażenie js, które ma zostać wyświetlone w template.
+    Musi to być pojedyńcze wyrażenie, które zwraca jedną wartość. Można używać skrótów logicznych typu :
+    {{ var || 'Hello' }} lub {{ var ? 'Hello' : 'Bye' }}-->
 
-  <h1>{{ header }}</h1>
   <ul>
     <li v-for='{id, label} in items' :key='id'>{{label}}</li>
     <li v-for='({id, label}, index) in items' :key="id">{{ index }} {{ id }}.{{ label }} </li>
   </ul>
+  <!-- Tak powstaje dynamicznie generowana lista, która składa się z eksportowanych elementów z sekcji <script>.
+      Aby iterować po elementach tablicy lub obiektu, należy posłużyć się dyrektywą v-for='item in items' :key:'item'. Dodanie atrybutu :key nie jest wymagane ale jest dobrą praktyką przy dużych aplikacjach-->
+
+  <input v-model.lazy="newItem" type="text" placeholder="Add an item">
+    <ul>
+      {{ newItem }}
+    </ul>
+
 
   </div>
   
@@ -23,7 +34,8 @@ export default {
         {id: 1, label: 'kielbasa'},
         {id: 2, label: 'keczup'},
         {id: 3, label: 'majonez'}
-      ]
+      ],
+      newItem: '',
     }
     }
   }
@@ -40,3 +52,5 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+<!-- Comment -->
